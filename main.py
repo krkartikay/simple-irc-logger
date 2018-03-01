8d9a2b2ccec4a716e956fd1b611188ec6eb21e43
Kumar Kartikay <krkartikay@gmail.com>
2018-03-01 13:26:09 +0530

    write directly to logfile

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import time
 channel = "#testit"
 server = "irc.freenode.net"
 nickname = "logger_bot"
- 
+filename = "log.txt"
+
 irc = IRC()
 irc.connect(server, channel, nickname)
-  
-while 1:
-    text = irc.get_text()
-    if "PRIVMSG" in text and channel in text:
-        name = text[text.index(':')+1:text.index('!')]
-        message = text[text.index(channel):]
-        message = message[message.index(':')+1:-1]
-        print(time.asctime()+"\t"+name+":\t"+message)
-    elif "PART" in text:
-        name = text[text.index(':')+1:text.index('!')]
-        print(time.asctime()+"\t<-- "+name+" left")
-    elif "JOIN" in text:
-        name = text[text.index(':')+1:text.index('!')]
-        print(time.asctime()+"\t--> "+name+" has joined")
+
+with open(filename,'w') as f:
+    while 1:
+        text = irc.get_text()
+        if "PRIVMSG" in text and channel in text:
+            name = text[text.index(':')+1:text.index('!')]
+            message = text[text.index(channel):]
+            message = message[message.index(':')+1:-1]
+            f.write(time.asctime()+"\t"+name+":\t"+message+'\n')
+        elif "PART" in text:
+            name = text[text.index(':')+1:text.index('!')]
+            f.write(time.asctime()+"\t<-- "+name+" left"+'\n')
+        elif "JOIN" in text:
+            name = text[text.index(':')+1:text.index('!')]
+            f.write(time.asctime()+"\t--> "+name+" has joined"+'\n')
